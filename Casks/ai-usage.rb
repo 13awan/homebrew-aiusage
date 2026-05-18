c67b9bc143645f01455ee555b4d99d30813b38af
13awan <13awan.us@gmail.com>
2026-05-18 10:11:36 +0800

    Fix known issues

--- Casks/ai-usage.rb
+++ Casks/ai-usage.rb
@@ -1,6 +1,6 @@
 cask "ai-usage" do
-    version "1.0"
-    sha256 "5e05d10ded2ba8d50c0370af059970a87ac607b47a096f32e12d7598be1dae03"
+    version "1.1"
+    sha256 "d7088302d6ebfdc37c96d0d51a4387831a98fe04db6d4672983305cd71cf3137"
     url "https://github.com/13awan/homebrew-aiusage/raw/main/releases/download/v#{version}/AI.Usage.zip"
     name "AI Usage"
     desc "Cursor AI Usage"
